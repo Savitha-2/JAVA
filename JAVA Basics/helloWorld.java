51d155b466c25bfa467c4d5f86051831a3627bfb
Savitha-2 <savithabalegar86@gmail.com>
2025-12-12 17:02:05 +0530

    Initial commit with Java folder structure

---
+++ JAVA Basics/helloWorld.java
@@ -0,0 +1,12 @@
+//class name should match file name
+// Java Is Case-Sensitive
+//main ≠ Main
+//System ≠ system
+//String ≠ string
+
+class helloWorld {
+     
+public static void main(String args[]){
+        System.out.println("Hello, World!");
+    }
+} 
