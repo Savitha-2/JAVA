68f7183a0fcad003e24c3fca807189d8188c4ebb
Savitha-2 <savithabalegar86@gmail.com>
2026-01-17 14:55:08 +0530

    Static

--- 7_Static_ThisPointer_Constructor/staticBlocks.java
+++ 7_Static_ThisPointer_Constructor/staticBlocks.java
@@ -10,11 +10,14 @@ class StaticBlock {
     StaticBlock() {
         System.out.println("Constructor Called");
     }
+     void display() {
+        System.out.println("Display Method Called");
+    }
 }
 public class staticBlocks {
     public static void main(String[] args) {
         StaticBlock sb = new StaticBlock();
-        
+        sb.display();
 
         
         
